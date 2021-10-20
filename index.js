206e5d5ffc63e9ee5d0ff8a8798dafc1e39a3af3
Rayden Lee <rayden.lee@ultrafast.co.nz>
2021-10-21 10:46:32 +1300

    add named export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const linzGeodetic = require('./www/linz-geodetic');
 const linzCoordType = require('./www/linz-coordtype');
 
-module.exports = Object.assign({}, linzGeodetic, linzCoordType);
+module.exports.LINZ = Object.assign({}, linzGeodetic, linzCoordType);
